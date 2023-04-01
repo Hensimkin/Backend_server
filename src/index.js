580bd7c38bc9315860b644dd6316a56321747e25
Hen Simkin <hensi1@ac.sce.ac.il>
2023-04-01 16:28:28 +0300

    server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//     console.log('A new request');
+//     res.send('Hello from server main page');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
+
+
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
+
+
 
 app.get('/', (req, res) => {
-    console.log('A new request');
-    res.send('Hello from server main page');
+    res.send('Hello from backend server!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
