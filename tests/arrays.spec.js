945505f76e0c334df2f3f8767c63528530cc5200
Hen Simkin <hensi1@ac.sce.ac.il>
2023-04-01 12:40:49 +0300

    server

--- tests/arrays.spec.js
+++ tests/arrays.spec.js
@@ -4,7 +4,7 @@ describe("arrays",()=>{
     describe("#sort",()=>{
         it("sorting names array",()=>{
             var names=["adam","hen"];
-            expect(names.sort()).to.be.eql(["hen","adam"]);
+            expect(names.sort((a, b) => b.localeCompare(a))).to.be.eql(["hen","adam"]);
         });
     });
 });
