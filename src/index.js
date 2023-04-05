a7f9aa7f7b5f219b1917d6653daa7c711c35e274
Omer Halfon <omerhalfon1234@gmail.com>
2023-04-05 12:10:38 +0300

    response from server

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Add this line to parse JSON requests
 
+
+
 app.get('/', (req, res) => {
     console.log('A new request');
     res.send('Hello from server main page');
@@ -16,14 +18,22 @@ app.get('/', (req, res) => {
 
 app.post('/post_email', async (req, res) => {
     let { email } = req.body;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email))
+        res.send('Please enter a valid email address');
+    else
+        res.send('Email received'); // Add this line to send a response back to the client
     console.log(email);
-    res.send('Email received'); // Add this line to send a response back to the client
 });
 
 app.post('/post_password', async (req, res) => {
     let { password } = req.body;
+    if (password.length < 8)
+        res.send('Password must be at least 8 characters long');
+    else
+        res.send('Password received'); // Add this line to send a response back to the client
     console.log(password);
-    res.send('Password received'); // Add this line to send a response back to the client
+
 });
 
 app.listen(port, () => {
